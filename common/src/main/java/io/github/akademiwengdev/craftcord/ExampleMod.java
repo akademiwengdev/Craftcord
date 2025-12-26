8c212561e6bbf8972e72044765bcb2b98ede0ff3
Mednoob <ahmadyaqdhan123@gmail.com>
2025-12-26 13:34:47 +0700

    feat: init project

---
+++ common/src/main/java/io/github/akademiwengdev/craftcord/ExampleMod.java
@@ -0,0 +1,9 @@
+package io.github.akademiwengdev.craftcord;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "craftcord";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
